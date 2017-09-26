91d18195918f10936284e71d821b7a9c663c6000
DIR\lionell.c.briones <lionell.c.briones@accenture.com>
2017-09-26 14:45:58 +0800

    Add route otherwise

--- routing/app.js
+++ routing/app.js
@@ -2,16 +2,20 @@
     
     angular.module("app", ["ngRoute"])
     .config(function($routeProvider) {
-        $routeProvider.when("/",{
+        $routeProvider.when("/:firstName/:middleName/:lastName",{
             templateUrl: "app.html",
             controller: "AppCtrl",
             controllerAs: "app"
         })
+        .when('/cookies', {
+            template: 'Nomnomonom'
+        })
+        .otherwise('/')
     })
-    .controller("AppCtrl", function() {
+    .controller("AppCtrl", function($routeParams) {
         var self = this;
 
-        self.message = "The app routing is working!";
+        self.message = "Hi " + $routeParams.firstName + ' ' + $routeParams.middleName + ' ' + $routeParams.lastName;
     });
     
 })()
