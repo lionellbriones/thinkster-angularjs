190b058aab1101b8c0f39788df8852c4d0fd78dc
DIR\lionell.c.briones <lionell.c.briones@accenture.com>
2017-09-26 10:17:23 +0800

    Added logic for View Dom

---
+++ view-dom/app.js
@@ -0,0 +1,25 @@
+angular.module("app", []);
+
+angular.module("app")
+.directive("dumbPassword", function() {
+    var validElement = angular.element('<div>{{ model.input }}</div>');
+    
+    var link = function(scope) {
+        scope.$watch("model.input", function(value) {
+            if(value === "password") {
+                validElement.toggleClass("alert-box alert");
+            }
+        });
+    };
+
+    return {
+        restrict: "E",
+        replace: true,
+        templateUrl: "dumbpass.html",
+        compile: function(tElem) {
+            tElem.append(validElement);
+
+            return link;
+        }
+    }
+})
